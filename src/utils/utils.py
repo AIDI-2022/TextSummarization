dd6fd3747d07da5526f4547d172f046da1295394
Navaneeth J <navaneethjawahar333@gmail.com>
2022-02-23 19:52:14 -0500

    added pre processing in utils

---
+++ src/utils/utils.py
@@ -0,0 +1,22 @@
+import nltk
+import pickle
+import re
+import numpy as np
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+
+
+def preprocessor(text):
+    replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
+    bad_symbols_re = re.compile('[^0-9a-z #+_]')
+    stopwords_set = set(stopwords.words('english'))
+
+    text = text.lower()
+    text = replace_by_space_re.sub(' ', text)
+    text = bad_symbols_re.sub('', text)
+    text = ' '.join([x for x in text.split() if x and x not in stopwords_set])
+
+    return text.strip()
+
